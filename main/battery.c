87dd57855719bf580bdeb2d26c9574613ebd94ed
ShotaAk <macakasit@gmail.com>
2019-08-30 11:14:20 +0900

    printf to ESP_LOGD

--- main/battery.c
+++ main/battery.c
@@ -24,27 +24,27 @@ static void check_efuse()
 {
     //Check TP is burned into eFuse
     if (esp_adc_cal_check_efuse(ESP_ADC_CAL_VAL_EFUSE_TP) == ESP_OK) {
-        printf("eFuse Two Point: Supported\n");
+        ESP_LOGD("eFuse Two Point: Supported\n");
     } else {
-        printf("eFuse Two Point: NOT supported\n");
+        ESP_LOGD("eFuse Two Point: NOT supported\n");
     }
 
     //Check Vref is burned into eFuse
     if (esp_adc_cal_check_efuse(ESP_ADC_CAL_VAL_EFUSE_VREF) == ESP_OK) {
-        printf("eFuse Vref: Supported\n");
+        ESP_LOGD("eFuse Vref: Supported\n");
     } else {
-        printf("eFuse Vref: NOT supported\n");
+        ESP_LOGD("eFuse Vref: NOT supported\n");
     }
 }
 
 static void print_char_val_type(esp_adc_cal_value_t val_type)
 {
     if (val_type == ESP_ADC_CAL_VAL_EFUSE_TP) {
-        printf("Characterized using Two Point Value\n");
+        ESP_LOGD("Characterized using Two Point Value\n");
     } else if (val_type == ESP_ADC_CAL_VAL_EFUSE_VREF) {
-        printf("Characterized using eFuse Vref\n");
+        ESP_LOGD("Characterized using eFuse Vref\n");
     } else {
-        printf("Characterized using Default Vref\n");
+        ESP_LOGD("Characterized using Default Vref\n");
     }
 }
 
