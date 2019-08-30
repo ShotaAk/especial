6270c51ce250033c1162ddbcb5ab33f024ac4b5c
ShotaAk <macakasit@gmail.com>
2019-08-30 10:58:07 +0900

    Add Kconfig file

--- main/indicator.c
+++ main/indicator.c
@@ -14,8 +14,8 @@
 #include "esp_log.h"
 static const char *TAG="Indicator";
 
-#define GPIO_LED0 22
-#define GPIO_LED1 23
+#define LED0_GPIO CONFIG_LED0_GPIO
+#define LED1_GPIO CONFIG_LED1_GPIO
 
 void TaskIndicator(void *arg){
     gpio_config_t io_conf;
@@ -24,7 +24,7 @@ void TaskIndicator(void *arg){
     //set as output mode
     io_conf.mode = GPIO_MODE_OUTPUT;
     //bit mask of the pins that you want to set,e.g.GPIO18/19
-    io_conf.pin_bit_mask = ((1ULL<<GPIO_LED0) | (1ULL<<GPIO_LED1));
+    io_conf.pin_bit_mask = ((1ULL<<LED0_GPIO) | (1ULL<<LED1_GPIO));
     //disable pull-down mode
     io_conf.pull_down_en = 0;
     //disable pull-up mode
@@ -33,8 +33,8 @@ void TaskIndicator(void *arg){
     gpio_config(&io_conf);
     /* 省略 */
 
-    gpio_set_level(GPIO_LED0, 0);
-    gpio_set_level(GPIO_LED1, 0);
+    gpio_set_level(LED0_GPIO, 0);
+    gpio_set_level(LED1_GPIO, 0);
 
     bool ledToggle = false;
 
@@ -42,21 +42,20 @@ void TaskIndicator(void *arg){
     while(1){
         if(gBatteryVoltage > LOW_BATTERY_VOLTAGE){
             // バッテリー電圧があれば、モードを表示
-            gpio_set_level(GPIO_LED1, gIndicatorValue & 0x01);
-            gpio_set_level(GPIO_LED0, (gIndicatorValue >> 1) & 0x01);
+            gpio_set_level(LED1_GPIO, gIndicatorValue & 0x01);
+            gpio_set_level(LED0_GPIO, (gIndicatorValue >> 1) & 0x01);
             vTaskDelay(100 / portTICK_PERIOD_MS);
         }else{
             // 電圧がなければ、点滅させる
             if(ledToggle){
-                gpio_set_level(GPIO_LED0, 1);
-                gpio_set_level(GPIO_LED1, 1);
+                gpio_set_level(LED0_GPIO, 1);
+                gpio_set_level(LED1_GPIO, 1);
             }else{
-                gpio_set_level(GPIO_LED0, 0);
-                gpio_set_level(GPIO_LED1, 0);
+                gpio_set_level(LED0_GPIO, 0);
+                gpio_set_level(LED1_GPIO, 0);
             }
 
             ledToggle = !ledToggle;
-            // printf("03_TaskIndicator\n");
             vTaskDelay(100 / portTICK_PERIOD_MS);
         }
     }
