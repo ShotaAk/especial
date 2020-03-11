f0f77faf68c40094ab4a47d39fdc87425ac6cee7
ShotaAk <macakasit@gmail.com>
2020-03-11 11:19:49 +0900

    remove extern from header file

--- examples/6_motion_tracking/main/icm20648.h
+++ examples/6_motion_tracking/main/icm20648.h
@@ -3,9 +3,9 @@
 
 namespace ICM20648
 {
-    extern void init(const int mosi_io_num, const int miso_io_num, 
+    void init(const int mosi_io_num, const int miso_io_num,
         const int sclk_io_num, const int cs_io_num);
-    extern int read_who_am_i(void);
+    int read_who_am_i(void);
 }
 
 #endif /* !ICM20648_H */
